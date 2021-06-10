8fc5ac53316e7524a0a2005fe63c2547241d9855
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-06-10 00:00:18 -0300

    craindo projeto guidepress
    criando rotas, tabelas e middlewares

---
+++ guidepress/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const connection = require("./database/database")
+
+//Import das controllers
+const articlesController = require('./controllers/ArticlesController')
+const categoriesController = require('./controllers/CategoriesController')
+
+const port = 8080;
+const app = express();
+
+connection.authenticate()
+    .then(() => console.log('Conexão efetuada com o Banco de Dados'))
+    .catch(() => console.log('Erro ao conectar com o banco de dados'))
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Configurando middlewares das controllers
+app.use('/categories', categoriesController)
+app.use('/articles', articlesController)
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+
+app.listen(port, () => console.log(`Server running at https://localhost:${port}`))
