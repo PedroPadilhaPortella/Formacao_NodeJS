445ffc30027a1d7911efcd8f9519a4b3b848f663
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-07-02 18:23:04 -0300

    tdd com login

--- compartilhamento-imagens/src/app.js
+++ compartilhamento-imagens/src/app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const app = express()
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose')
+
+const app = express()
+const jwtSecret = 'tdd-project';
 
 const UserSchema = require('./models/User')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/guiapictures', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/guiapics', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {})
     .catch((err) => console.error(err));
 
@@ -30,13 +34,45 @@ app.post('/users', async (req, res) => {
             return res.status(400).json({ error: 'Este email já está em uso por outro usuário.'})
         }
 
-        const user = new User({ name, email, password });
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = new User({ name, email, password: hash });
 
         await user.save();
-        return res.json({ name, email, password })
+        return res.json({ name, email })
     } catch (err) {
         return res.status(500).send(err)
     }
 });
+
+app.post('/login', async(req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({'email': email})
+    if (!user) {
+        return res.status(403).json({ errors: { email: 'Email não cadastrado.'}})
+    }
+    
+    const isPassword = await bcrypt.compare(password, user.password);
+    if(!isPassword) {
+        return res.status(403).json({ errors: { password: 'Senha incorreta.'}})
+    }
+
+    jwt.sign({ email, name: user.name, id: user._id }, jwtSecret, { expiresIn: '48h' }, (err, token) => {
+        if(err) {
+            console.error(err)
+            return res.status(500).end()
+        }
+        return res.json({ token });
+    });
+});
+
+app.delete('/users/:name', async (req, res) => {
+    const name = req.params.name;
+    await User.deleteMany({ 'name': name});
+    res.status(200).end();
+});
+
 
 module.exports = app;
--- compartilhamento-imagens/test/user.test.js
+++ compartilhamento-imagens/test/user.test.js
@@ -3,43 +3,92 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('User', () => {
+    const admin = { name: 'admin', email: 'admin@gmail.com', password: 'admin123' }
+
+    beforeAll(async() => {
+        return request.post('/users').send(admin)
+            .then(() => {})
+            .catch((error) => console.error(error))
+    });
+
+    afterAll(async() => {
+        return request.delete(`/users/${admin.name}`)
+            .then(async() => {
+                return request.delete('/users/user')
+                .then(() => {})
+                .catch((error) => fail(error))
+            })
+            .catch((error) => fail(error))
+    });
 
     describe('Cadastro de Usuário', () => {
         test('deve cadastrar um usuário', async () => {
             const email = `user${Date.now()}@gmail.com`
-            const user = { name: 'user', email, senha: 'user123' }
+            const user = { name: 'user', email, password: 'user123' }
 
             return request.post('/users').send(user)
                 .then((response) => {
                     expect(response.statusCode).toBe(200)
                     expect(response.body.email).toBe(email)
-                }).catch((error) => fail(error));
+                })
+                .catch((error) => fail(error))
         });
 
         test('deve rejeitar o cadastro de um usuário com os dados vazios', async () => {
-            const user = { name: '', email: '', senha: '' }
+            const user = { name: '', email: '', password: '' }
 
             return request.post('/users').send(user)
                 .then((response) => {
                     expect(response.statusCode).toBe(400)
-                }).catch((error) => fail(error));
+                })
+                .catch((error) => fail(error))
         });
 
         test('deve rejeitar o cadastro de um usuário com email que já existe', async () => {
             const email = `user${Date.now()}@gmail.com`
-            const user = { name: 'user', email, senha: 'user123' }
+            const user = { name: 'user', email, password: 'user123' }
 
             return request.post('/users').send(user)
-                .then((response) => {
+                .then(async(response) => {
                     expect(response.statusCode).toBe(200)
                     expect(response.body.email).toBe(email)
 
                     return request.post('/users').send(user)
-                        .then((response) => {
-                            expect(response.statusCode).toBe(400);
-                            expect(response.body.error).toBe('Este email já está em uso por outro usuário.');
+                        .then((res) => {
+                            expect(res.statusCode).toBe(400);
+                            expect(res.body.error).toBe('Este email já está em uso por outro usuário.');
                         }).catch((error) => fail(error));
-                }).catch((error) => fail(error));
+                })
+                .catch((error) => fail(error))
         });
     });
+
+    describe('Autenticação de Usuário', () => {
+        test('deve gerar um token Jwt quando efetuar o login', async() => {
+            return request.post('/login').send(admin)
+                .then((response) => {
+                    expect(response.statusCode).toBe(200);
+                    expect(response.body.token).toBeDefined();
+                })
+                .catch((error) => fail(error))
+        });
+
+        test('deve rejeitar o login de um usuário não cadastrado', async() => {
+            return request.post('/login').send({ email: 'fakeemail', password: '12345' })
+                .then((response) => {
+                    expect(response.statusCode).toBe(403);
+                    expect(response.body.errors.email).toBe('Email não cadastrado.');
+                })
+                .catch((error) => fail(error));
+        });
+
+        test('deve rejeitar o login de um usuário com uma senha incorreta', async() => {
+            return request.post('/login').send({ email: admin.email, password: '12345' })
+                .then((response) => {
+                    expect(response.statusCode).toBe(403);
+                    expect(response.body.errors.password).toBe('Senha incorreta.');
+                })
+                .catch((error) => fail(error))
+        });
+    })
 });
